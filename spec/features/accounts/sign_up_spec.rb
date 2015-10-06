00854f86a0767f202634fdeb7e967185a374947c
webfoxxx <max@webfox.com>
2015-10-06 16:07:57 -0700

    feature #2: associating accounts with their owners

--- spec/features/accounts/sign_up_spec.rb
+++ spec/features/accounts/sign_up_spec.rb
@@ -5,8 +5,12 @@ feature "Accounts" do
     visit subscribem.root_path
     click_link "Account Sign Up"
     fill_in "Name", :with => "Test"
+    fill_in "Email", :with => "subscribem@example.com"
+    fill_in "Password", :with => "password"
+    fill_in "Password confirmation", :with => "password"
     click_button "Create Account"
     success_message = "KudosBro Your account has been successfully created."
     expect(page).to have_content(success_message)
+    expect(page).to have_content("Signed in as subscribem@example.com")
   end
 end
