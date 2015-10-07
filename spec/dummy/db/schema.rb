4bb33be7ef6632eb87e0cd97e88977bc7efc7e43
webfoxxx <max@webfox.com>
2015-10-07 14:04:50 -0700

    Feature #3: Add subdomains to accounts

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -11,14 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151002215800) do
+ActiveRecord::Schema.define(version: 20151007203804) do
 
   create_table "subscribem_accounts", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "owner_id"
+    t.string   "subdomain"
   end
+
+  add_index "subscribem_accounts", ["subdomain"], name: "index_subscribem_accounts_on_subdomain"
 
   create_table "subscribem_users", force: :cascade do |t|
     t.string   "email"
