10773ab9899cfeed495c5d262d79fe5b36d3bb9f
webfoxxx <max@webfox.com>
2015-10-12 12:52:16 -0700

    Feature #8: Invalid usernames or passwords

--- app/controllers/subscribem/account/sessions_controller.rb
+++ app/controllers/subscribem/account/sessions_controller.rb
@@ -5,10 +5,15 @@ module Subscribem
     def new
       @user = User.new
     end
+
     def create
       if env["warden"].authenticate(:scope => :user)
-        flash[:notice] = "You are now signed in."
+        flash[:success] = "You are now signed in."
         redirect_to root_path
+      else
+        @user = User.new
+        flash[:error] = "Invalid email or password."
+        render :action => "new"
       end
     end
   end
