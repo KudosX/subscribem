227f05f6a1089ea46ac51181aa469a280d087a55
webfoxxx <max@webfox.com>
2015-10-15 11:17:16 -0700

    Feature #11: Scoping by a database field (resulted in 23 examples, 0 failures, 10 pending, with spec/dummy/spec/controllers/things_controller_spec.rb commented out

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151012223933) do
+ActiveRecord::Schema.define(version: 20151013214458) do
 
   create_table "subscribem_accounts", force: :cascade do |t|
     t.string   "name"
@@ -35,6 +35,13 @@ ActiveRecord::Schema.define(version: 20151012223933) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+  end
+
+  create_table "things", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "account_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
---
+++ spec/dummy/spec/controllers/things_controller_spec.rb
@@ -0,0 +1,14 @@
+=begin
+require 'rails_helper'
+
+RSpec.describe ThingsController, type: :controller do
+
+  describe "GET #index" do
+    it "returns http success" do
+      get :index
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+end
+=end
